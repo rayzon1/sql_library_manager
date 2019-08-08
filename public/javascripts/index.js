f8ce0632f345a5d2837715147c99afa97f4245ae
Gerardo Keys <jotojry@icloud.com>
2019-08-08 11:15:10 -0700

    full functionality complete

---
+++ public/javascripts/index.js
@@ -0,0 +1,70 @@
+const search = document.getElementById("searchBar");
+const pageDiv = document.getElementById("paginationContainer");
+const pageButtons = document.getElementsByClassName("buttonPage");
+const tableList = document.getElementsByClassName("tdata");
+const ul = createEl("ul", "class", "listContainer");
+const pageNumbers = Math.ceil(tableList.length / 15);
+
+/**
+ * Event listener for search bar. Will display matches to search.
+ */
+search.addEventListener("keyup", e => {
+  [...tableList].forEach(val => {
+    val.innerHTML.toLowerCase().includes(e.target.value)
+      ? (val.style.display = "")
+      : (val.style.display = "none");
+  });
+});
+
+/**
+ * Function to create elements.
+ */
+function createEl(elem, attr, name) {
+  let element = document.createElement(elem);
+  element.setAttribute(attr, name);
+  return element;
+}
+
+/**
+ * Will show 15 book listings per page. Shows page
+ * based on page number selected.
+ */
+function showPage(list, page) {
+  let end = page * 15;
+  let start = end - 15;
+  for (let i = 0; i < list.length; i++) {
+    if (i >= start && i < end) {
+      list[i].style.display = "";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+}
+
+/**
+ * Creates the pagination buttons and displays below the body 
+ * of the page.
+ */
+for (let i = 1; i <= pageNumbers; i++) {
+  const li = createEl("li", "class", "listItem");
+  const a = createEl("a", "href", "#");
+  const button = createEl("button", "class", "buttonPage");
+  li.appendChild(a);
+  button.append(i);
+  a.appendChild(button);
+  ul.append(li);
+  pageDiv.append(ul);
+}
+
+/**
+ * Will add event listener to each pagination button 
+ * displayed.
+ */
+[...pageButtons].forEach(val => {
+  val.addEventListener("click", e => {
+    showPage(tableList, e.target.innerText);
+  });
+});
+
+showPage(tableList, 1);
+
--- routes/allBooks.js
+++ routes/allBooks.js
@@ -1,88 +1,100 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Books = require('../models').Books;
+var Books = require("../models").Books;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    Books.findAll()
-        .then(book => {
-            res.render('index', { books: book });
-        });
+/* GET library listing. */
+router.get("/", function(req, res, next) {
+  Books.findAll({order: [["Title", "ASC"]]})
+    .then(book => {
+      res.render("index", { books: book });
+    })
+    .catch(err => console.error("There was an error", err.status));
 });
 
-/* GET new book. */
-router.get('/new', function(req, res, next) {
-    Books.findAll()
-        .then(book => {
-            res.render('addNew', { books: book });
-        })
-    
+/* GET new book creation page. */
+router.get("/new", function(req, res, next) {
+  Books.findAll()
+    .then(book => {
+      res.render("new-book", { books: book });
+    })
+    .catch(err => console.error("There was an error", err.status));
 });
 
-/* POST new book. */
-router.post('/new', function(req, res, next) {
-    Books.create(req.body)
-        .then(book => res.redirect('/books/' + book.id));
+/* POST newly created book. */
+router.post("/new", function(req, res, next) {
+  Books.create(req.body)
+    .then(book => res.redirect("/books/" + book.id))
+    .catch(err => {
+        if(err.name === "SequelizeValidationError") {
+            res.render("new-book", {
+              books: Books.build(req.body), 
+              errors: err.errors
+            });
+          } else {
+            throw err;
+          }
+    });
 });
 
 /* GET selected book by id. */
-router.get('/:id', function(req, res, next) {
-    Books.findByPk(req.params.id)
-        .then(book => {
-            res.render('show', { books: book })
-        })
-        .catch(err => {
-            res.send(505, err)
-        })
+router.get("/:id", function(req, res, next) {
+  Books.findByPk(req.params.id)
+    .then(book => {
+      res.render("show", { books: book });
+    })
+    .catch(err => console.error("There was an error", err));
 });
 
 /* GET selected book edit page */
-router.get('/:id/edit', function(req, res, next) {
-    Books.findByPk(req.params.id)
-        .then(book => {
-            res.render('edit', { books: book })
-        })
-}); 
-
-/* PUT update selected book */
-router.put('/:id', function(req, res, next) {
-    Books.findByPk(req.params.id)
-        .then(book => {
-            if(book) {
-                return book.update(req.body)
-            } else {
-                res.send(404)
-            }
-        })
-        .then(book => {
-            console.log('Book updated.')
-            res.redirect('/books/' + book.id)
-        })
+router.get("/:id/edit", function(req, res, next) {
+  Books.findByPk(req.params.id)
+    .then(book => {
+      res.render("update-book", { books: book });
+    })
+    .catch(err => console.error("There was an error", err));
 });
 
-//TODO: Add get route for delete view to confirm delete submission
+/* PUT update selected book then redirect to show book by id. */
+router.put("/:id", function(req, res, next) {
+  Books.findByPk(req.params.id)
+    .then(book => {
+      return book.update(req.body);
+    })
+    .then(book => {
+      console.log("Book updated.");
+      res.redirect("/books/" + book.id);
+    })
+    .catch(err => {
+        let book = Books.build(req.body);
+        book.id = req.params.id;
+        if(err.name === "SequelizeValidationError") {
+            res.render("update-book", {
+              books: book, 
+              errors: err.errors
+            });
+          } else {
+            throw err;
+          }
+    });
+});
 
-router.get('/:id/delete', function(req, res, next) {
-    Books.findByPk(req.params.id)
-        .then(book => {
-            res.render('delete', { books: book})
-        })
-})
+/* GET delete page */
+router.get("/:id/delete", function(req, res, next) {
+  Books.findByPk(req.params.id)
+    .then(book => {
+      res.render("delete", { books: book });
+    })
+    .catch(err => console.error("There was an error", err));
+});
 
-
-
-
-
-router.delete('/:id', function(req, res, next) {
-    Books.findByPk(req.params.id)
-        .then(book => {
-            if(book) {
-                return book.destroy();
-            } else {
-                send(404);
-            }
-        })
-        .then(() => res.redirect('/books'))
-})
+/* DELETE selected book will be deleted */
+router.delete("/:id", function(req, res, next) {
+  Books.findByPk(req.params.id)
+    .then(book => {
+      return book.destroy();
+    })
+    .then(() => res.redirect("/books"))
+    .catch(err => console.error("There was an error", err));
+});
 
 module.exports = router;
